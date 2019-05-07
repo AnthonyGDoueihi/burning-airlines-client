f880ea36c8ffb9cebccc5bd05f83c5812d6e28cb
Max Reeves <maxreeves@Maxs-MacBook-Pro.local>
2019-05-07 21:37:43 +1000

    Nav Component v.2

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import axios from 'axios'
-import './ham.css'
 
 class Nav extends Component {
 	constructor() {
@@ -9,10 +8,10 @@ class Nav extends Component {
 
 	burgerToggle() {
   let linksEl = document.querySelector('.narrowLinks');
-  if (linksEl.style.display === 'block') {
-            linksEl.style.display = 'none';
-        } else {
+  if (linksEl.style.display === 'none') {
             linksEl.style.display = 'block';
+        } else {
+            linksEl.style.display = 'none';
         }
 }
 
@@ -21,20 +20,11 @@ class Nav extends Component {
 			<nav>
 			  <div>
 			      <div>
-			          <a href="#" onClick={this.burgerToggle}>
-								<svg class="ham hamRotate ham1"
-								viewBox="0 0 100 100"
-								width="80"
-								onclick="this.classList.toggle('active')">
-								<path
-        				class="line top"
-        				d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40" />
-								</svg>
-								</a>
+			          <a href="#" onClick={this.burgerToggle}>MENU</a>
 			        </div>
 			    </div>
 			    <div>
-			        <div className="narrowLinks">
+			        <div className="narrowLinks" style={{display: 'none'}}>
 			           <a href="./Search" onClick={this.burgerToggle}>Search</a>
 										<br/>
 			            <span><a href="./Flights" onClick={this.burgerToggle}>Flights</a></span>
@@ -48,6 +38,5 @@ class Nav extends Component {
 		)
 	}
 };
-
 
 export default Nav;
