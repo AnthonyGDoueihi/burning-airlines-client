40c9e58f08bf089a094299f0a87e8cade3e14731
Max Reeves <maxreeves@Maxs-MacBook-Pro.local>
2019-05-08 09:29:40 +1000

    Bootstrap

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react'
-
-import Nav from './Nav'
 import Header from './Header'
-
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button';
+<link
+  rel="stylesheet"
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+  crossorigin="anonymous"
+/>
 
 class Search extends Component {
 	constructor(){
@@ -12,7 +18,18 @@ class Search extends Component {
 	render () {
 		return (
 			<div className='container'>
-        <Nav />
+			<Navbar bg="dark" variant="dark">
+		    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+		    <Nav className="mr-auto">
+		      <Nav.Link href="#home">Home</Nav.Link>
+		      <Nav.Link href="#features">Features</Nav.Link>
+		      <Nav.Link href="#pricing">Pricing</Nav.Link>
+		    </Nav>
+		    <Form inline>
+		      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+		      <Button variant="outline-info">Search</Button>
+		    </Form>
+		  </Navbar>
         <Header />
         <SearchForm />
         <h3>Flight Search Results</h3>
