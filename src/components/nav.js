1134709cf90a2e13dde6ed8b17d8fa2fbc223010
Max Reeves <maxreeves@Maxs-MacBook-Pro.local>
2019-05-07 17:38:40 +1000

    axios install partial navbar

---
+++ src/components/nav.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+
+class Nav extends Component {
+	render () {
+		return (
+			<div>
+				<nav>
+					<ul>
+					
+					</ul>
+				</nav>
+			</div>
+		);
+	}
+};
+
+}
+
+
+export default Airplanes;
