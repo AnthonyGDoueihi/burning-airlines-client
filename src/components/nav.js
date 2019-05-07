cadacc1126af33591ac4771b81c617066dd73a26
Max Reeves <maxreeves@Maxs-MacBook-Pro.local>
2019-05-07 19:15:39 +1000

    Nav Merge

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,19 +1,31 @@
 import React, {Component} from 'react'
 import axios from 'axios'
+import ReactDOM from 'react-dom';
 
 class Nav extends Component {
 	render () {
 		return (
-			<div>
-				<nav>
-					<ul>
-					
-					</ul>
-				</nav>
-			</div>
-		);
+			<nav>
+			  <div className="navWide">
+			      <div className="wideDiv">
+			          <a href="#">Link 1</a>
+			          <a href="#">Link 2</a>
+			          <a href="#">Link 3</a>
+			        </div>
+			    </div>
+			    <div className="navNarrow">
+			      <i className="fa fa-bars fa-2x"></i>
+			        <div className="narrowLinks">
+			            <a href="#">Link 1</a>
+			            <a href="#">Link 2</a>
+			            <a href="#">Link 3</a>
+			        </div>
+			  </div>
+			</nav>
+		)
 	}
 };
+ReactDOM.render(<NavComponent />, document.querySelector('navbar'));
 
 }
 
