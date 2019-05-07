82b0e3e3570f21375de99974c676907d60ce8b39
Anthony <agdoueihi@gmail.com>
2019-05-07 17:29:02 +1000

    Routes Created

---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { HashRouter as Router, Route } from 'react-router-dom'
+
+import Search from './components/Search'
+import Airplanes from './components/Airplanes'
+import Flights from './components/Flights'
+import Booking from './components/Booking'
+
+const Routes = (
+  <Router>
+    <div>
+      <Route exact path='/' component={ Search } />
+      <Route exact path='/airplanes' component={ Airplanes } />
+      <Route exact path='/flights' component={ Flights } />
+      <Route path='/flight/:index' component={ Booking } />
+    </div>
+  </Router>
+);
+
+export default Routes;
