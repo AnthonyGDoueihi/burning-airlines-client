e95de80b80710fa4b26a36d9e1cfd76cb6533130
Anthony <agdoueihi@gmail.com>
2019-05-09 10:44:10 +1000

    Updates

---
+++ precache-manifest.2f8e8c9d930241094a071d60905a29f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "701675b0610190e4d71a0242092372d7",
+    "url": "/burning-airlines-client/index.html"
+  },
+  {
+    "revision": "7cb9e17ffac91607869b",
+    "url": "/burning-airlines-client/static/css/main.eaae61b6.chunk.css"
+  },
+  {
+    "revision": "07a7d56a2e04a6c3dd69",
+    "url": "/burning-airlines-client/static/js/2.b72f7f32.chunk.js"
+  },
+  {
+    "revision": "7cb9e17ffac91607869b",
+    "url": "/burning-airlines-client/static/js/main.292deaae.chunk.js"
+  },
+  {
+    "revision": "515d6a9089d89487faea",
+    "url": "/burning-airlines-client/static/js/runtime~main.7f3b5825.js"
+  },
+  {
+    "revision": "447fa692371eca31154217cd1c7f0d4d",
+    "url": "/burning-airlines-client/static/media/burning-airlines.447fa692.png"
+  }
+]);
